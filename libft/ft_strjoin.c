db1a9279ec35eecab69d6f44cc6933564ec3f6d8
nakamoto daichi <nakamotodaichi@nakamotonoMacBook-Pro.local>
2026-05-07 18:46:48 +0900

    オーバーフローの修正

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,48 +6,51 @@
 /*   By: nakamotodaichi <nakamotodaichi@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/07 15:49:51 by username          #+#    #+#             */
-/*   Updated: 2026/05/07 17:10:22 by nakamotodai      ###   ########.fr       */
+/*   Updated: 2026/05/07 18:44:52 by nakamotodai      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include "libft.h"
 #include <stdlib.h>
+#include <limits.h>
 
-size_t	ft_strlen(char const *str)
+static int	check_overflow(size_t s1_len, size_t s2_len)
 {
-	size_t	i;
+	if (s1_len > SIZE_MAX - s2_len - 1)
+		return (1);
+	return (0);
+}
 
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
+static void	copy_str(char *dst, char const *src, size_t *i)
+{
+	size_t	j;
+
+	j = 0;
+	while (src[j])
+	{
+		dst[*i] = src[j];
+		(*i)++;
+		j++;
+	}
 }
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	size_t	s1_len;
 	size_t	s2_len;
-	size_t	i1;
-	size_t	i2;
+	size_t	i;
 	char	*str;
 
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
+	if (check_overflow(s1_len, s2_len))
+		return (NULL);
 	str = malloc(sizeof(char) * (s1_len + s2_len + 1));
 	if (!str)
 		return (NULL);
-	i1 = 0;
-	while (i1 < s1_len)
-	{
-		str[i1] = s1[i1];
-		i1++;
-	}
-	i2 = 0;
-	while (i2 < s2_len)
-	{
-		str[i1 + i2] = s2[i2];
-		i2++;
-	}
-	str[i1 + i2] = '\0';
+	i = 0;
+	copy_str(str, s1, &i);
+	copy_str(str, s2, &i);
+	str[i] = '\0';
 	return (str);
 }
