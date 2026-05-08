7038fe0cc704156c8b74cf1c18fe08be16c19b43
Daichi Nakamoto <nakamotodaich@icloud.com>
2026-05-09 01:42:45 +0900

    ft_isalphaの引数の型がおかしかったので修正

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -3,16 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isalpha.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dnakamot <dnakamot@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nakamotodaichi <nakamotodaichi@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/23 00:45:48 by dnakamot          #+#    #+#             */
-/*   Updated: 2026/04/25 21:54:40 by dnakamot         ###   ########.fr       */
+/*   Updated: 2026/05/09 01:41:53 by nakamotodai      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-int	ft_isalpha(char c)
+int	ft_isalpha(int c)
 {
 	return (('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z'));
 }
+
+// int main(void)
+// {
+// 	int c;
+// 	c=1;
+// 	printf("%d",ft_isalpha(c));
+// }
